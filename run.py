f6b9180d3a3ec48f8ad55dc76f409cd974c5eb3f
lucasvianasilva <lucas.vianadasilva@outlook.com>
2025-09-25 14:45:23 -0300

    Realizando alterações importantes

--- run.py
+++ run.py
@@ -1,17 +1,11 @@
 # run.py
-
-# Importar o pacote 'backend' é a primeira coisa que fazemos.
-# Isso garante que o código no arquivo 'backend/__init__.py' (incluindo o eventlet.monkey_patch())
-# seja executado antes de qualquer outra importação da nossa aplicação.
 import backend
-
-# Agora que o patch foi aplicado, podemos importar o restante com segurança.
 from backend.app import create_app
 from backend.extensions import socketio
 
+# Cria as instâncias do app e do socketio no escopo global do arquivo
 app = create_app()
 
+# O bloco de execução __main__ é usado apenas para desenvolvimento local
 if __name__ == '__main__':
-    # Usamos host='0.0.0.0' para garantir que o servidor seja acessível
-    # a partir de fora do container no ambiente de produção.
     socketio.run(app, host='0.0.0.0', port=5000, debug=True)
