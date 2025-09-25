4e92b404048302ebf782db1ec908771a1447a73f
lucasvianasilva <lucas.vianadasilva@outlook.com>
2025-09-25 12:46:35 -0300

    Aplica monkey_patch do eventlet para corrigir erro de lock

--- run.py
+++ run.py
@@ -1,16 +1,17 @@
 # run.py
+
+import eventlet
+eventlet.monkey_patch()
+
 import os
 import sys
 
-# --- INÍCIO DA SOLUÇÃO FORÇADA ---
-# Pega o caminho absoluto da pasta onde o run.py está (ex: C:\...\\PROJETO VIZINHO AMIGO)
+# Pega o caminho absoluto da pasta onde o run.py está
 project_root = os.path.dirname(os.path.abspath(__file__))
 # Adiciona a pasta raiz do projeto ao início do caminho de busca do Python
 sys.path.insert(0, project_root)
-# --- FIM DA SOLUÇÃO FORÇADA ---
 
-# Agora, com o caminho do projeto explicitamente definido,
-# as importações devem funcionar sem erro.
+# Agora, com o patch aplicado, as outras importações são seguras.
 from backend.app import create_app
 from backend.extensions import socketio
 
